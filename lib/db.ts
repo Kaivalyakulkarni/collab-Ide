76b954ec187ce9c917ac6dcf32db21a3db3e5ba7
Kaivalyakulkarni <kaivalyakulkarni2011@gmail.com>
2026-05-05 17:45:47 +0530

    fix: lazy init PrismaClient to fix build

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,15 @@
+import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+function getPrismaClient() {
+  if (!globalForPrisma.prisma) {
+    const connectionString = process.env.DATABASE_URL!
+    const adapter = new PrismaPg({ connectionString })
+    globalForPrisma.prisma = new PrismaClient({ adapter })
+  }
+  return globalForPrisma.prisma
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+export const prisma = getPrismaClient()
